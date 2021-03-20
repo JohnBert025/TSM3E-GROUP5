da6ed8ac8f1844849125efc63fddd42331251f1a
Lesterjohn-13 <79785623+Lesterjohn-13@users.noreply.github.com>
2021-03-20 08:51:21 +0800

    Add files via upload

---
+++ Person (1).java
@@ -0,0 +1,13 @@
+public class Person { 
+    private String pName;
+   
+    public Person(String pName){
+   this.pName=pName;
+   }
+    public void name (String pName){
+    this.pName =pName;
+    }
+    public String name(){
+    return pName;}
+}
+
